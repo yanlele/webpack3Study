a9e9c9debc20d7c00a4cb718f1a6b4ad037d9dc4
yanlele <331393627@qq.com>
2017-11-17 08:20:44 +0800

    单入口出口配置

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,12 @@
-module.exports={
-    entry:{},
-    output: {
+const path=require('path');
 
+module.exports={
+    entry:{
+        entry:'./src/entry.js'
+    },
+    output: {
+        path:path.resolve(__dirname,'dist'),
+        filename: "bundle.js"
     },
     module:{
 
