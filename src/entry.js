04fdc2855c80aed583f0ffa64ddb2d89f6456a9b
yanlele <331393627@qq.com>
2017-11-19 20:07:53 +0800

    js的模块化

--- src/entry.js
+++ src/entry.js
@@ -1,8 +1,12 @@
 import css from './css/index.css';
 import less from './css/black.less'
 
+import lele from './lele';
+
 {
     let str='hello webpack!!';
 
     document.getElementById('title').innerHTML=str;
 }
+
+lele();
